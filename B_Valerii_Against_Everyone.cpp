e95a1f693fe2606014a854c6fd993ab7c94d49ea
SumonR-14 <sumonrayhan01926@gmail.com>
2026-07-28 14:26:45 +0600

    solved new 3 pblm

---
+++ B_Valerii_Against_Everyone.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        unordered_set<int>s;
+        for(int i=0; i<n; i++){
+            int x; cin >> x;
+            s.insert(x);
+        }
+        if(s.size()<n){
+            cout<<"YES" <<endl;
+        }
+        else cout<<"NO" <<endl;
+    }
+    
+    return 0;
+}
