a460e687ed1e9c9cc851fac96890ac58ef1af7d1
SumonR-14 <sumonrayhan01926@gmail.com>
2026-08-30 12:33:22 +0600

    solved a new pblm

---
+++ A_Odd_Eraser.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t; cin >> t;
+   while (t--)
+   {
+      int n; cin >> n;
+      vector<long long> a(n+1);
+      for(int i=1; i<=n; i++){
+        cin >> a[i];
+      }
+      cout<<gcd(a[1],a[n]) <<endl;
+   }
+   
+    return 0;
+}
